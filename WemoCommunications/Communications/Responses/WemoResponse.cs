8ce322cf34490a4016f501452ec9c73beb0dfd38
seanksullivan <seanksullivan@gmail.com>
2018-03-12 17:38:23 -0700

    Removed Communications csproj

--- WemoCommunications/Communications/Responses/WemoResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Communications.Responses
-{
-    public class WemoResponse
-    {
-        public string Description { get; set; }
-        public string StatusCode { get; set; }
-        public string ResponseBody { get; set; }
-    }
-}
