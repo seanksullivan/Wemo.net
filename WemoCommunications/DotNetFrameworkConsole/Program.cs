183e68f8599ef86d95504cd79ccecd9fc2dc17cd
seanksullivan <seanksullivan@gmail.com>
2018-03-16 14:39:55 -0700

    Converted the library from .NetCore to .Net Standard; added 'test' console apps as exmaples for .Net Framework and .NetCore usage.

---
+++ WemoCommunications/DotNetFrameworkConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using WemoNet;
+
+namespace DotNetFrameworkConsole
+{
+    class Program
+    {
+        /// <summary>
+        /// Run the console app and pass-in a valid IP Address.  e.g.: "http://192.168.1.5"
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Toggle a swith for a given IP address
+            var ipAddress = args.FirstOrDefault();
+
+            // Verify the ipAddress is an actual ip address
+            var uriConvertSuccess = Uri.TryCreate(ipAddress, UriKind.Absolute, out Uri uri);
+
+            if (!uriConvertSuccess)
+            {
+                Console.WriteLine("Not a valid Ip Address");
+                return;
+            }
+
+            var wemo = new Wemo();
+
+            // ACT
+            var result = wemo.ToggleWemoPlugAsync(ipAddress).GetAwaiter().GetResult();
+        }
+    }
+}
